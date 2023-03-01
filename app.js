73792248b3421b9a358d8effd11ae4da321a21ed
Your Name01~ <you@example.com>
2023-03-01 21:27:51 +0600

    #routing - Added routing between pages

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const authRoutes = require('./routes/auth')
+const analyticsRoutes = require('./routes/auth')
+const categoryRoutes = require('./routes/auth')
+const orderRoutes = require('./routes/auth')
+const positionRoutes = require('./routes/auth')
+const app = express()
+
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+app.use('/api/auth', authRoutes)
+app.use('/api/analytics', analyticsRoutes)
+app.use('/api/category', categoryRoutes)
+app.use('/api/order', orderRoutes)
+app.use('/api/position', positionRoutes)
+
+
+module.exports = app
