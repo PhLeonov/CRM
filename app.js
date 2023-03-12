0ab6fd62774c3e40b8e0c6c7b0a425fb3c7e2771
Your Name01~ <you@example.com>
2023-03-12 15:51:15 +0600

    #api - Creating an API

--- app.js
+++ app.js
@@ -11,13 +11,14 @@ const keys = require('./config/keys')
 const app = express()
 
 mongoose.connect(keys.mongoURI)
-  .then(() => console.log('MongoDB connected!'))
+  .then(() => console.log('MongoDB connected.'))
   .catch(error => console.log(error))
 
 app.use(passport.initialize())
 require('./middleware/passport')(passport)
 
 app.use(require('morgan')('dev'))
+app.use('/uploads', express.static('uploads'))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(require('cors')())
