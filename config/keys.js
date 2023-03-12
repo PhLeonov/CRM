6e5391572d2470270cb0b3bddd959bd2460a8026
Your Name01~ <you@example.com>
2023-03-12 15:15:33 +0600

    #category - Added category creation

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://fil:bxYz4KiE2do1GyKz@cluster0.sqy8pxz.mongodb.net/?retryWrites=true&w=majority',
-    jwt: 'dev-jwt'
+    mongoURI: 'mongodb+srv://filipp1:bxYz4KiE2do1GyKz@cluster0.newm7yb.mongodb.net/?retryWrites=true&w=majority',
+    jwt: 'dev-jwt' 
 }
